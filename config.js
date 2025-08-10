dc95f78bd3a3491efa9408b928003bc483067617
0osodjix0 <verba.ilya10@gmail.com>
2025-08-10 13:48:19 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const Config = {
+    API_KEY: '188ce352-ffb7-429a-81df-0a96d598921e',
+    MAP_CENTER: [55.751244, 37.618423], // Москва
+    DEFAULT_ZOOM: 12,
+    MAX_BOOKING_DAYS: 30,
+    API_ENDPOINTS: {
+        MASTERS: '/api/masters',
+        SCHEDULE: '/api/schedule',
+        BOOKINGS: '/api/bookings'
+    }
+};
